84fe8624b671a54b9bc26bc25b05eadd57ea0a0a
TREMAIN MANNION - STUDENT <g00367973@atu.ie>
2026-03-03 16:42:13 +0000

    Wk7Lab11 updates

---
+++ src/controllers/statusController.js
@@ -0,0 +1,8 @@
+exports.getStatus = (req, res) => {
+  res.json({
+    status: "Online",
+    message: "AWS Backend is reachable!",
+    owner: "Fergus Downey",
+    timestamp: new Date()
+  });
+};
---
+++ src/routes/productRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+// Import the controller functions
+const productController = require('../controllers/productController');
+
+// Notice we just use '/' here, because we will mount this router on '/products' later
+router.get('/', productController.getProducts);
+router.post('/', productController.createProduct);
+router.put('/:id', productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
+
+module.exports = router;
